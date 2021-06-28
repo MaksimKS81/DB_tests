1830a1f792ba067a370fe93a85fef134da61f38d
MaksimKS81 <krivolapov.maksim@gmail.com>
2021-06-28 10:09:33 +0200

    MongoDB test script

---
+++ MongoDB_test.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 25 13:15:58 2021
+
+@author: admin
+"""
+
+import pymongo
+
+from random import randint
+#Step 1: Connect to MongoDB - Note: Change connection string as needed
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+db=client.business
+#Step 2: Create sample data
+names = ['Kitchen','Animal','State', 'Tastey', 'Big','City','Fish', 'Pizza','Goat', 'Salty','Sandwich','Lazy', 'Fun']
+company_type = ['LLC','Inc','Company','Corporation']
+company_cuisine = ['Pizza', 'Bar Food', 'Fast Food', 'Italian', 'Mexican', 'American', 'Sushi Bar', 'Vegetarian']
+for x in range(1, 501):
+    business = {
+        'name' : names[randint(0, (len(names)-1))] + ' ' + names[randint(0, (len(names)-1))]  + ' ' + company_type[randint(0, (len(company_type)-1))],
+        'rating' : randint(1, 5),
+        'cuisine' : company_cuisine[randint(0, (len(company_cuisine)-1))] 
+    }
+    #Step 3: Insert business object directly into MongoDB via insert_one
+    result=db.reviews.insert_one(business)
+    #Step 4: Print to the console the ObjectID of the new document
+    print('Created {0} of 500 as {1}'.format(x,result.inserted_id))
+#Step 5: Tell us that you are done
+print('finished creating 500 business reviews')
